c0279bc4e4fec52e9bb5b59847b9b22300a1e737
ijbd <ijbd@umich.edu>
2023-01-23 13:04:28 -0500

    Simplest model through EnergySystem.

--- assetra/core.py
+++ assetra/core.py
@@ -1,59 +1,141 @@
+from abc import ABC
 from logging import getLogger
-from abc import abstractmethod, ABC
 from typing import List
+
+# external libraries
+import dask as da
 
 log = getLogger(__name__)
 
-class EnergyUnit(ABC):
-	@abstractmethod
-	def get_hourly_generation(self):
-		'''Return a single time-series'''
+
+### RESOURCE CONTRIBUTION
+
+class ResourceContributionMetric:
+	'''Class responsible for evaluating the resource contribution
+	of one energy system object to another.'''
+	def __init__(
+		self):
 		pass
 
-class EnergySystem:
-	def __init__(self):
-		self._energy_units = []
+### RESOURCE ADEQUACY
 
-	def add_energy_unit(self, energy_unit):
-		self._energy_units.append(energy_unit)
+class ResourceAdequacyMetric:
+	'''Class responsible for evaluating resource adequacy of
+	a ProbabilisticSimulation object.'''
+	def __init__(
+		self):
+		pass
 
-	def remove_energy_unit(self, energy_unit):
-		self._energy_units.remove(energy_unit)
+### PROBABILISTIC SIMULATION
 
-	def get_hourly_generation(self):
-		'''Return an array of time-series for all energy_units'''
-		for energy_source in self.energy_sources:
-			get_hourly_generation(energy_unit)
+class BulkProbabilisticSimulation:
+	'''Class responsible for creating/storing the Monte Carlo
+	trials for BulkEnergySystem objects.'''
+	def __init__(
+		self,
+		bulk_energy_system: BulkEnergySystem,
+		num_monte_carlo_trials: int,
+		num_time_steps: int):
+		pass
+
+	def run(self):
+		pass
 
 class ProbabilisticSimulation:
+	'''Class responsible for creating/storing the Monte Carlo
+	trials for EnergySystem objects.'''
 	def __init__(
 		self, 
 		energy_system: EnergySystem,
-		num_monte_carlo_iterations: int,
+		num_monte_carlo_trials: int,
 		num_time_steps: int):
-		# initialize state variables
-		self.energy_system = energy_system
-		self.num_monte_carlo_iterations = num_monte_carlo_iterations
-		self.num_time_steps = num_time_steps
+		pass
 		
 	def run(self):
-		self.hourly_generation_matrix = da.zeros((
-			self.num_monte_carlo_iterations,
-			self.num_time_steps,
-			self.num_generators))
-		self.hourly_net_generation_matrix = da.zeros((
-			self.num_monte_carlo_iterations,
-			self.num_time_steps))
+		pass
 
-		for it in range(num_monte_carlo_iterations):
-			self.hourly_generation_matrix[it] = \
-				self.energy_system.get_hourly_generation()
-			
-		self.hourly_net_generation_matrix = \
-			da.sum(self.hourly_net_generation_matrix[iterations])	
+### CORE 
 
-class ResourceAdequacyMetric:
-	pass
+class BulkEnergySystem:
+	'''Class responsible for balancing capacity between
+	energy systems.'''
+	def __init__(self, energy_systems: List[EnergySystem]):
+		self.energy_systems = energy_systems
 
-class ResourceContributionMetric:
-	pass
+	def get_hourly_capacity(self):
+		pass
+
+class EnergySystem:
+	'''Class responsible for managing energy units.'''
+	def __init__(
+		self,
+		name: str):
+		self.energy_units = []
+
+	def add_unit(self, energy_unit):
+		self.energy_units.append(energy_unit)
+
+	def remove_unit(self, energy_unit):
+		self.energy_units.remove(energy_unit)
+
+	def get_hourly_capacity_by_unit(self):
+		'''Returns the hourly capacity of each generating unit
+		in the energy system.'''
+		hourly_capacity_matrix = da.zeros(
+			(self.num_generators, 8760))
+		hourly_net_capacity = da.zeros(8760)
+		for i, energy_unit in enumerate(self.energy_units):
+			hourly_capacity_matrix[i] = energy_unit.get_hourly_capacity()
+			hourly_net_capacity += hourly_capacity_matrix[i]
+
+		return hourly_capacity_matrix
+
+class EnergyUnit(ABC):
+	def __init__(
+		self,
+		name: str,
+		nameplate_capacity: float):
+		self._name = name
+		self._nameplate_capacity = nameplate_capacity
+
+	@abstractmethod
+	def get_hourly_capacity
+		'''Returns a single instance of the hourly capacity of the 
+		generating unit.'''
+
+class StochasticUnit:
+	'''Class responsible for returning capacity profile of 
+	stochastically-sampled units (i.e. generators).'''
+	def __init__(
+		self,
+		name: str,
+		nameplate_capacity: float,
+		hourly_capacity: da.typing.ArrayLike,
+		hourly_forced_outage_rate: da.typing.ArrayLike
+		):
+		EnergyUnit.__init__(self, name, nameplate_capacity)
+		self._hourly_capacity = hourly_capacity
+		self._hourly_forced_outage_rate = hourly_forced_outage_rate
+
+	def get_hourly_capacity(self):
+		hourly_outage_samples = da.random.random(8760)
+		hourly_capacity_instance = da.where(
+			outages < self._hourly_forced_outage_rate,
+			self._hourly_capacity,
+			0)
+		return hourly_capacity_instance
+
+class DemandUnit:
+	'''Class responsible for returning capacity profile of 
+	fixed demand units (i.e. system loads).'''
+	def __init__(
+		self,
+		name: str,
+		hourly_demand: da.typing.ArrayLike
+		):
+		EnergyUnit.__init__(self, name, nameplate_capacity)
+		self._hourly_demand = hourly_capacity
+		self._hourly_forced_outage_rate = hourly_forced_outage_rate
+
+	def get_hourly_capacity(self):
+		return -(self._hourly_demand)
