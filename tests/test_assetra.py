15d1f18d1d21b8206aeb985a85cf2273dd7f3822
ijbd <ijbd@umich.edu>
2023-01-24 14:55:05 -0500

    Numpy probabilistic analysis method.

--- tests/test_assetra.py
+++ tests/test_assetra.py
@@ -21,12 +21,7 @@ class TestCore(unittest.TestCase):
 		# test
 		expected = -hourly_demand
 		observed = u.get_hourly_capacity(start_hour=0, end_hour=3)
-		self.assertTrue(
-			np.array_equal(
-				expected,
-				observed
-				)
-			)
+		self.assertTrue(np.array_equal(expected, observed))
 
 	def test_stochastic_unit_1(self):
 		'''Capacity of stochastic unit with FOR=0 is full capacity.'''
@@ -43,12 +38,7 @@ class TestCore(unittest.TestCase):
 		# test
 		expected = hourly_capacity
 		observed = u.get_hourly_capacity(start_hour=0, end_hour=3)
-		self.assertTrue(
-			np.array_equal(
-				expected,
-				observed
-				)
-			)
+		self.assertTrue(np.array_equal(expected, observed))
 
 	def test_stochastic_unit_2(self):
 		'''Capacity of stochastic unit with FOR=1 is zero.'''
@@ -65,12 +55,7 @@ class TestCore(unittest.TestCase):
 		# test
 		expected = np.array([0, 0, 0])
 		observed = u.get_hourly_capacity(start_hour=0, end_hour=3)
-		self.assertTrue(
-			np.array_equal(
-				expected,
-				observed
-				)
-			)
+		self.assertTrue(np.array_equal(expected, observed))
 
 	def test_energy_system_1(self):
 		'''Energy units can be added and removed from systems.'''
@@ -109,9 +94,7 @@ class TestCore(unittest.TestCase):
 		e.add_unit(u1)
 		expected = np.array([[0, -1]])
 		observed = e.get_hourly_capacity_by_unit(0,2)
-		self.assertTrue(
-			np.array_equal(expected, observed)
-		)
+		self.assertTrue(np.array_equal(expected, observed))
 
 		# sub-test 1
 		e.add_unit(u2)
@@ -119,13 +102,36 @@ class TestCore(unittest.TestCase):
 			[0, -1], 
 			[-3, -4]])
 		observed = e.get_hourly_capacity_by_unit(0,2)
-		self.assertTrue(
-			np.array_equal(expected, observed)
+		self.assertTrue(np.array_equal(expected, observed))
+	
+class TestProbabilisticAnalysis(unittest.TestCase):
+
+	def test_probabilistic_simulation_1(self):
+		'''Probabilistic simulation should generate hourly capacity matrix.'''
+		from assetra.core import EnergySystem, DemandUnit
+		from assetra.probabilistic_analysis import ProbabilisticSimulation
+		e = EnergySystem('test_system')
+		u1 = DemandUnit(
+			name='test_unit',
+			hourly_demand=np.array([0, 1])
+			)
+		u2 = DemandUnit(
+			name='test_unit',
+			hourly_demand=np.array([3, 4])
+			)
+		e.add_unit(u1)
+		e.add_unit(u2)
+		
+		p = ProbabilisticSimulation(e, 0, 2, 4)
+
+		# test
+		p.run()
+		expected = np.array(
+			[[[0, -1],
+			[-3, -4]]]*4
 		)
-
-
-
-
+		observed = p.hourly_capacity_matrix
+		self.assertTrue(np.array_equal(expected, observed))
 
 
 if __name__ == '__main__':
