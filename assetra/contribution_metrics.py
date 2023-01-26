335dcbdd1bc45686cd5c7dfa0f1297fd57915f1b
ijbd <ijbd@umich.edu>
2023-01-26 13:30:09 -0500

    Replace tabs with spaces.

--- assetra/contribution_metrics.py
+++ assetra/contribution_metrics.py
@@ -4,12 +4,12 @@ from abc import abstractmethod, ABC
 from assetra.probabilistic_analysis import ProbabilisticSimulation
 
 class ResourceContributionMetric(ABC):
-	'''Class responsible for evaluating the resource contribution
-	of one energy system object to another.'''
-	def __init__(
-		self):
-		pass
+    '''Class responsible for evaluating the resource contribution
+    of one energy system object to another.'''
+    def __init__(
+        self):
+        pass
 
-	@abstractmethod
-	def get_resource_contribution(self):
-		pass
+    @abstractmethod
+    def get_resource_contribution(self):
+        pass
--- assetra/probabilistic_analysis.py
+++ assetra/probabilistic_analysis.py
@@ -6,41 +6,41 @@ from assetra.core import EnergySystem, BulkEnergySystem
 import numpy as np
 
 class ProbabilisticSimulation:
-	'''Class responsible for creating/storing the Monte Carlo
-	trials for EnergySystem objects.'''
-	def __init__(
-		self, 
-		energy_system: EnergySystem,
-		start_hour: int,
-		end_hour: int,
-		trial_size: int):
-		self._energy_system = energy_system
-		self._start_hour = start_hour
-		self._end_hour = end_hour
-		self._trial_size = trial_size
+    '''Class responsible for creating/storing the Monte Carlo
+    trials for EnergySystem objects.'''
+    def __init__(
+        self, 
+        energy_system: EnergySystem,
+        start_hour: int,
+        end_hour: int,
+        trial_size: int):
+        self._energy_system = energy_system
+        self._start_hour = start_hour
+        self._end_hour = end_hour
+        self._trial_size = trial_size
 
-	@property
-	def hours(self):
-		return self._end_hour - self._start_hour
-		
-	def run(self):
-		self.hourly_capacity_matrix = np.zeros((self._trial_size, 
-			self._energy_system.size, self._end_hour - self._start_hour))
-		for trial in range(self._trial_size):
-			self.hourly_capacity_matrix[trial] = \
-				self._energy_system.get_hourly_capacity_by_unit(
-					self._start_hour, self._end_hour)
+    @property
+    def hours(self):
+        return self._end_hour - self._start_hour
+        
+    def run(self):
+        self.hourly_capacity_matrix = np.zeros((self._trial_size, 
+            self._energy_system.size, self._end_hour - self._start_hour))
+        for trial in range(self._trial_size):
+            self.hourly_capacity_matrix[trial] = \
+                self._energy_system.get_hourly_capacity_by_unit(
+                    self._start_hour, self._end_hour)
 
 class BulkProbabilisticSimulation:
-	'''Class responsible for creating/storing the Monte Carlo
-	trials for BulkEnergySystem objects.'''
-	def __init__(
-		self,
-		bulk_energy_system: BulkEnergySystem,
-		start_hour: int,
-		end_hour: int,
-		trial_size: int):
-		pass
+    '''Class responsible for creating/storing the Monte Carlo
+    trials for BulkEnergySystem objects.'''
+    def __init__(
+        self,
+        bulk_energy_system: BulkEnergySystem,
+        start_hour: int,
+        end_hour: int,
+        trial_size: int):
+        pass
 
-	def run(self):
-		pass
+    def run(self):
+        pass
