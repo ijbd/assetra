c80c83168eb9044a2c42cbb7e396ff0897fa5cd1
ijbd <ijbd@umich.edu>
2023-03-08 13:47:59 -0500

    Remove project title beneath logo.

--- docs/conf.py
+++ docs/conf.py
@@ -96,6 +96,7 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_logo = 'asset-logo.png'
+logo_only = True
 
 
 # -- Options for HTMLHelp output ---------------------------------------
